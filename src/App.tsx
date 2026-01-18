7e431d64fcf74d0d80b7b2a3c4f7f13a3c33db36
inovafani <inoveka@gmail.com>
2026-01-18 14:24:56 +0700

    fix ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import DataImage from './data';
-import { listTools, listProyek } from './data';
+import DataImage from './data.ts';
+import { listTools, listProyek } from './data.ts';
 
 function App() {
   return (
